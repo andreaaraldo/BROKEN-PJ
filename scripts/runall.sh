c8eb354c95220b516be4fb689b06360ef867ce89
Giuseppe <giuseros85@gmail.com>
2013-05-09 13:24:21 +0200

    Fixed icons

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 main=./ccnSim
 
-runs=`$main -x General | awk '/runs/{print $4}'`
-opp_runall -j2 $main -u Cmdenv -r 0..$((runs-1))
+runs=`$main -x General $1 | awk '/runs/{print $4}'`
+opp_runall -j2 $main -u Cmdenv -r 0..$((runs-1)) $1
