aa0f9339d77da4d6c892c260e59e216972ff7523
Andrea Araldo <andrea.araldo@gmail.com>
2013-11-25 18:11:53 +0100

    .

---
+++ src/error_handling.cc
@@ -0,0 +1,38 @@
+/*
+ * ccnSim is a scalable chunk-level simulator for Content Centric
+ * Networks (CCN), that we developed in the context of ANR Connect
+ * (http://www.anr-connect.org/)
+ *
+ * People:
+ *    Giuseppe Rossini (lead developer, mailto giuseppe.rossini@enst.fr)
+ *    Andrea Araldo (lead developer, mailto andrea.araldo@gmail.com)
+ *    Raffaele Chiocchetti (developer, mailto raffaele.chiocchetti@gmail.com)
+ *    Dario Rossi (occasional debugger, mailto dario.rossi@enst.fr)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+//<aa>
+#include "error_handling.h"
+#include <sstream>
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+
+void severe_error(char* source_file_name, int code_line, std::string error_message){
+	cout << source_file_name << " " << code_line << ": ERROR: " << error_message << endl;
+	//exit(3);
+}
+//</aa>
