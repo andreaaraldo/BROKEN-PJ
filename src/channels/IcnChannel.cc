2513137fd10e5e23a3f9312967158bbcb5a0bd71
andrea <andrea@asus-ubuntu.(none)>
2013-12-31 03:01:35 +0100

    computing link cost

--- src/channels/IcnChannel.cc
+++ src/channels/IcnChannel.cc
@@ -13,22 +13,43 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
+//<aa>
 #include "IcnChannel.h"
+#include "error_handling.h"
+#include "cmodule.h"
+#include "ccnsim.h" // for CCN_D
 
 
 Define_Channel(IcnChannel);
 
 void IcnChannel::initialize()
 {
-        EV << "ChannelWithCost initialized";
         cDatarateChannel::initialize();
         cost = par("cost");
+        double cost_asc = par("cost_asc");
+        double cost_desc = par("cost_desc");
+        
+        //cGate* sourceGate = getSourceGate();
+        //cModule* node_ = sourceGate->getOwnerModule();
+        //int source_index = node_->getIndex();
+        
+        int source_index = getSourceGate()->getOwnerModule()->getIndex();
+        int dest_index = getSourceGate()->getNextGate()->getOwnerModule()->getIndex();
+        cost = source_index < dest_index ? cost_asc : cost_desc;
+        
         effectiveCostID = registerSignal("effectiveCost");
+        std::stringstream msg; 
+		msg<<"cost:"<< cost;
+	    debug_message(__FILE__,__LINE__,msg.str().c_str() );
 }
 
 
 void IcnChannel::processMessage(cMessage *msg, simtime_t t, result_t& result)
 {
         cDatarateChannel::processMessage(msg,t,result);
-        emit(effectiveCostID,cost);
+        if( msg->getKind() == CCN_D ){
+        	// This is a data packet
+        	emit(effectiveCostID, cost );
+        }
 }
+//</aa>
