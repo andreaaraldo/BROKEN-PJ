9a98952574e74d9b951bfca28529d3c6a61ca0bc
andrea.araldo <andrea.araldo@gmail.com>
2014-04-21 22:34:48 +0200

    AA

--- scripts/result_processing/parse_results.m
+++ scripts/result_processing/parse_results.m
@@ -2,15 +2,16 @@
 
 priceratio_list={1,2,3,4,5,6,7,8,9,10};
 decision_list={"lce", "fix0.1", "prob_cache", "fix0.01","costprob0.1","costprob0.01"};
+id_rep_list=1:5; # list of seeds
 
 resultdir="~/software/ccnsim/results";
 network="simple_scenario";
 forwarding_="nrr";
 replacement_="lru";
 alpha_=0.8;
-ctlg_="10\\^8";
+ctlg_="10\\^3"; 
+ctlg_to_write_="1e3";
 csize_=10;
-id_rep_=0;
 
 i = 1;
 
@@ -19,6 +20,7 @@ disp("YOU SHOULD CHECK IF THE FILE EXISTS");
 
 for priceratio_idx = 1:length(priceratio_list)
 	for decision_idx = 1:length(decision_list)
+		for id_rep_ = id_rep_list
 			priceratio_ = priceratio_list{priceratio_idx};
 			decision_ = decision_list{decision_idx};
 
@@ -31,7 +33,7 @@ for priceratio_idx = 1:length(priceratio_list)
 			ctlg{i} = ctlg_;
 			csize{i} = csize_;
 			priceratio{i} = priceratio_;
-			id_rep{i} = id_rep_;
+			id_rep(i) = id_rep_;
 
 			string_to_search="p_hit ";
 			command = ["grep ","\"",string_to_search,"\""," ",filename," | awk \'{print $4}\' "];
@@ -58,78 +60,79 @@ for priceratio_idx = 1:length(priceratio_list)
 			# }CHECK RESULTS
 
 			i++;
-
+		endfor
 	endfor
 endfor
 
 
 ######### Matrix construction
 
-# Compute the priceratio_column, using whatever decision_ 
-decision_ = decision_list{1};
-idx =  strcmp(decision, decision_ );
-priceratio_column = cell2mat( priceratio(idx) );
+for id_rep_ = id_rep_list
+	# Compute the priceratio_column, using whatever decision_ 
+	decision_ = decision_list{1};
+	idx =  strcmp(decision, decision_ ) & ( id_rep == id_rep_ ) ;
+	priceratio_column = cell2mat( priceratio(idx) );
 
-p_hit_matrix = priceratio_column';
-total_cost_matrix = priceratio_column';
-hdistance_matrix = priceratio_column';
+	p_hit_matrix = priceratio_column';
+	total_cost_matrix = priceratio_column';
+	hdistance_matrix = priceratio_column';
 
-for decision_idx = 1:length(decision_list)
-	decision_ = decision_list{decision_idx};
-	idx =  strcmp(decision, decision_ ) & 1;
-	priceratio_column_for_check = cell2mat( priceratio(idx) );
-	if any(priceratio_column_for_check !=  priceratio_column)
-		error("price ratio is erroneous");
-	endif
-
-	p_hit_column = cell2mat( p_hit(idx) );
-	total_cost_column = cell2mat( total_cost(idx) );
-	hdistance_column = cell2mat( hdistance(idx) );
-
-	# CHECK DIMENSIONS{
-		if size(p_hit_matrix,1) != length(p_hit_column)
-			p_hit_matrix
-			p_hit_column
-			priceratio_column_for_check
-			decision_
-			error("Length of p_hit_column MUST bne equal to the row number of p_hit_matrix");
+	for decision_idx = 1:length(decision_list)
+		decision_ = decision_list{decision_idx};
+		idx =  strcmp(decision, decision_ ) & id_rep == id_rep_;
+		priceratio_column_for_check = cell2mat( priceratio(idx) );
+		if any(priceratio_column_for_check !=  priceratio_column)
+			error("price ratio is erroneous");
 		endif
-	# }CHECK DIMENSIONS
+
+		p_hit_column = cell2mat( p_hit(idx) );
+		total_cost_column = cell2mat( total_cost(idx) );
+		hdistance_column = cell2mat( hdistance(idx) );
+
+		# CHECK DIMENSIONS{
+			if size(p_hit_matrix,1) != length(p_hit_column)
+				p_hit_matrix
+				p_hit_column
+				priceratio_column_for_check
+				decision_
+				error("Length of p_hit_column MUST bne equal to the row number of p_hit_matrix");
+			endif
+		# }CHECK DIMENSIONS
 	
-	p_hit_matrix = [p_hit_matrix, p_hit_column'];
-	total_cost_matrix = [total_cost_matrix, total_cost_column'];
-	hdistance_matrix = [hdistance_matrix, hdistance_column'];
-endfor
+		p_hit_matrix = [p_hit_matrix, p_hit_column'];
+		total_cost_matrix = [total_cost_matrix, total_cost_column'];
+		hdistance_matrix = [hdistance_matrix, hdistance_column'];
+	endfor
 
-######### Enjoliver
-column_names{1} = {"priceratio"};
-for idx_decision = 1:length(decision_list)
-	column_names{ idx_decision+1 } = decision_list{ idx_decision };
-endfor
+	######### Enjoliver
+	column_names{1} = {"priceratio"};
+	for idx_decision = 1:length(decision_list)
+		column_names{ idx_decision+1 } = decision_list{ idx_decision };
+	endfor
 
-fixed_variables = { network; forwarding_; replacement_; alpha_; ctlg_; csize_; id_rep_ };
-fixed_variable_names = {"network"; "forwarding"; "replacement"; "alpha"; "ctlg";"csize"; "id_rep"};
+	fixed_variables = { network; forwarding_; replacement_; alpha_; ctlg_; csize_; id_rep_ };
+	fixed_variable_names = {"network"; "forwarding"; "replacement"; "alpha"; "ctlg";"csize"; "id_rep"};
 
-comment="";
+	comment="";
 
-metric="p_hit";
-matrix = p_hit_matrix;
-out_filename = ["~/Dropbox/shared_with_servers/icn14_runs/",metric,"-alpha_",num2str(alpha_),"-ctlg_",num2str(ctlg_),".dat"];
+	metric="p_hit";
+	matrix = p_hit_matrix;
+	out_filename = ["~/Dropbox/shared_with_servers/icn14_runs/",metric,"-alpha_",num2str(alpha_),"-ctlg_",ctlg_to_write_,"-seed_",num2str(id_rep_),".dat"];
 
-print_table(out_filename, matrix, column_names, fixed_variables,
-												fixed_variable_names, comment);
+	print_table(out_filename, matrix, column_names, fixed_variables,
+													fixed_variable_names, comment);
 
-metric="total_cost";
-matrix = total_cost_matrix;
-out_filename = ["~/Dropbox/shared_with_servers/icn14_runs/",metric,"-alpha_",num2str(alpha_),"-ctlg_",num2str(ctlg_),".dat"];
+	metric="total_cost";
+	matrix = total_cost_matrix;
+	out_filename = ["~/Dropbox/shared_with_servers/icn14_runs/",metric,"-alpha_",num2str(alpha_),"-ctlg_",ctlg_to_write_,"-seed_",num2str(id_rep_),".dat"];
 
-print_table(out_filename, matrix, column_names, fixed_variables,
-												fixed_variable_names, comment);
+	print_table(out_filename, matrix, column_names, fixed_variables,
+													fixed_variable_names, comment);
 
-metric="hdistance";
-matrix = hdistance_matrix;
-out_filename = ["~/Dropbox/shared_with_servers/icn14_runs/",metric,"-alpha_",num2str(alpha_),"-ctlg_",num2str(ctlg_),".dat"];
+	metric="hdistance";
+	matrix = hdistance_matrix;
+	out_filename = ["~/Dropbox/shared_with_servers/icn14_runs/",metric,"-alpha_",num2str(alpha_),"-ctlg_",ctlg_to_write_,"-seed_",num2str(id_rep_),".dat"];
 
-print_table(out_filename, matrix, column_names, fixed_variables,
-												fixed_variable_names, comment);
-
+	print_table(out_filename, matrix, column_names, fixed_variables,
+													fixed_variable_names, comment);
+endfor # id_rep for
