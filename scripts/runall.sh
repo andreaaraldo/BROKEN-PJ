7364343b353b48383d7b58e61db558693792a566
andrea_at_menabrea <andrea.araldo@gmail.com>
2014-05-14 11:33:38 +0200

    AA

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -3,5 +3,5 @@ main=./ccnSim
 NUM_OF_PROCESSORS=7;
 
 runs=`$main -x General $1 | awk '/runs/{print $4}'`
-cp ./omnetpp.ini /tmp/ccnsim_output-`date "+%Y-%m-%d_%Hh%M"`.ini
+cp ./omnetpp.ini /tmp/ccnsim_output-`date "+%Y-%m-%d_%Hh%M.%S"`.ini
 opp_runall -j$NUM_OF_PROCESSORS $main -u Cmdenv -r 0..$((runs-1)) $1 > /tmp/ccnsim_output-`date "+%Y-%m-%d_%Hh%M"`.log
