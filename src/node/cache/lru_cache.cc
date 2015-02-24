32c3c87e1a79a6fb09ae4895df8f48d4779d09f0
Andrea Araldo <andrea.araldo@gmail.com>
2015-02-24 16:29:35 +0100

    replacing the term cost with price

--- src/node/cache/lru_cache.cc
+++ src/node/cache/lru_cache.cc
@@ -91,10 +91,10 @@ void lru_cache::set_price_to_last_inserted_element(double price)
 	if ( statistics::record_cache_value )
 	{
 			std::stringstream ermsg; 
-			ermsg<<"set_price_to_last_inserted_element(..) is useful when you want to record the"
-				<< " cache_value; when statistics::record_cache_value is disabled this method "
-				<< " may be useless. Make sure you really need this method. If yes, disable this";
-				<< " error.If not, try to rethink your code";
+			ermsg<<"set_price_to_last_inserted_element(..) is useful when you want to record "<<
+				" the cache_value; when statistics::record_cache_value is disabled this method "<<
+				" may be useless. Make sure you really need this method. If yes, disable this"<<
+				" error.If not, try to rethink your code";
 			severe_error(__FILE__,__LINE__,ermsg.str().c_str() );
 	}
 	#endif
