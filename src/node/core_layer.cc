6a8fb3e227f2f8f177779ca738ae3ddf666928d2
Andrea Araldo <andrea.araldo@gmail.com>
2015-05-21 10:49:55 +0200

    AA

--- src/node/core_layer.cc
+++ src/node/core_layer.cc
@@ -257,11 +257,11 @@ void core_layer::handle_interest(ccn_interest *int_msg)
    bool cacheable = true;  // This value indicates whether the retrieved content will be cached.
     
     // Check if the meta-caching is 2-LRU. In this case, we need to lookup for the content ID inside the Name Cache.
-    string decision_policy = par("DS");
+    string decision_policy = ContentStore->par("DS");
 
     if (decision_policy.compare("two_lru")==0)
     {
-    	Two_Lru* tLruPointer = dynamic_cast<Two_Lru *> (ContentStore->get_decisor());
+    	Two_Lru* tLruPointer = (Two_Lru *) (ContentStore->get_decisor());
     	if (!(tLruPointer->name_to_cache(chunk)))	// The ID is not present inside the Name Cache, so the
     												// cacheable flag inside the PIT will be set to '0'.
     			cacheable = false;
